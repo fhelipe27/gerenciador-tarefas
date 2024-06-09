816c815a40650cd0bca5bab750ddf110db5bbad7
Fhelipe Pereira Alves <68212163+fhelipe27@users.noreply.github.com>
2024-06-09 02:03:23 -0300

    Adicionado serviço de email na seção de contato

--- src/main/java/br/com/unipac/gerenciadoratividadesapp/controllers/NavigationController.java
+++ src/main/java/br/com/unipac/gerenciadoratividadesapp/controllers/NavigationController.java
@@ -1,7 +1,10 @@
 package br.com.unipac.gerenciadoratividadesapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class NavigationController {
@@ -23,6 +26,11 @@ public class NavigationController {
     @GetMapping("/contato")
     public String contato() {
         return "contato";
+    }
+
+    @GetMapping("/contatoconfirmado")
+    public String contatoSucesso() {
+        return "contatoconfirmado";
     }
 
 }
