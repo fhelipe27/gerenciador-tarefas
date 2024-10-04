0e4a2d2357265ba981d4bb08e6cd4022f218ba02
Fhelipe Pereira Alves <68212163+fhelipe27@users.noreply.github.com>
2024-10-03 21:55:19 -0300

    mudanças no repository de tarefas

--- src/main/java/br/com/unipac/gerenciadoratividadesapp/repositories/TarefaRepository.java
+++ src/main/java/br/com/unipac/gerenciadoratividadesapp/repositories/TarefaRepository.java
@@ -1,17 +1,17 @@
-package br.com.unipac.gerenciadoratividadesapp.repositories;
+    package br.com.unipac.gerenciadoratividadesapp.repositories;
 
-import br.com.unipac.gerenciadoratividadesapp.models.Grupo;
-import br.com.unipac.gerenciadoratividadesapp.models.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
+    import br.com.unipac.gerenciadoratividadesapp.models.Grupo;
+    import br.com.unipac.gerenciadoratividadesapp.models.Tarefa;
+    import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+    import java.util.List;
 
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-    List<Tarefa> findByGrupo(Grupo grupo);
+    public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+        List<Tarefa> findByGrupo(Grupo grupo);
 
-    List<Tarefa> findByGrupoAndIsConcluida(Grupo grupo, boolean isConcluida);
+        List<Tarefa> findByGrupoAndIsConcluida(Grupo grupo, boolean isConcluida);
 
-    List<Tarefa> findByGrupoAndIsRemovida(Grupo grupo, boolean isRemovida);
+        List<Tarefa> findByGrupoAndIsRemovida(Grupo grupo, boolean isRemovida);
 
 
-}
+    }
