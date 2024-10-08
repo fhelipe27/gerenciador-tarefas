030deff7faaeeb0ee9b4b88ed408d3c715cf7f25
Fhelipe Pereira Alves <68212163+fhelipe27@users.noreply.github.com>
2024-10-07 21:47:02 -0300

    add todas as conquistas

--- src/main/java/br/com/unipac/gerenciadoratividadesapp/repositories/GrupoRepository.java
+++ src/main/java/br/com/unipac/gerenciadoratividadesapp/repositories/GrupoRepository.java
@@ -2,6 +2,9 @@ package br.com.unipac.gerenciadoratividadesapp.repositories;
 
 import br.com.unipac.gerenciadoratividadesapp.models.Grupo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GrupoRepository extends JpaRepository<Grupo, Long> {
 
@@ -9,4 +12,10 @@ public interface GrupoRepository extends JpaRepository<Grupo, Long> {
 
     public Grupo findByEmail(String email);
 
+    @Query("SELECT g.nome AS grupoNome, COUNT(gc.id) AS totalConquistasConcluidas " +
+            "FROM Grupo g JOIN g.grupoConquistas gc " +
+            "WHERE gc.conquistaConcluida = true " +
+            "GROUP BY g.idGrupo, g.nome " +
+            "ORDER BY totalConquistasConcluidas DESC")
+    List<Object[]> findGruposWithMostConquistas();
 }
--- src/main/java/br/com/unipac/gerenciadoratividadesapp/services/TarefaService.java
+++ src/main/java/br/com/unipac/gerenciadoratividadesapp/services/TarefaService.java
@@ -74,8 +74,41 @@ public class TarefaService {
 
     @Transactional
     public void deletarPorId(Long id) {
-        tarefaRepository.deleteById(id);
+        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id);
+        if (optionalTarefa.isPresent()) {
+            Tarefa tarefa = optionalTarefa.get();
+            Grupo grupo = tarefa.getGrupo();
+
+            // Remover a tarefa
+            tarefaRepository.delete(tarefa);
+
+            // Verifica se é a primeira tarefa removida permanentemente pelo grupo
+            List<Tarefa> tarefasRestantesDoGrupo = tarefaRepository.findByGrupo(grupo);
+
+            if (tarefasRestantesDoGrupo.size() == 0) {
+                // Buscar a conquista "Limpeza Definitiva"
+                Conquista limpezaDefinitivaConquista = conquistaRepository.findByNome("Limpeza Definitiva").orElse(null);
+
+                if (limpezaDefinitivaConquista != null) {
+                    // Verificar se a conquista já existe para o grupo
+                    GrupoConquista grupoConquista = grupoConquistaRepository.findByGrupoAndConquista(grupo, limpezaDefinitivaConquista);
+
+                    if (grupoConquista == null) {
+                        // Criar novo registro se não existir
+                        grupoConquista = new GrupoConquista();
+                        grupoConquista.setGrupo(grupo);
+                        grupoConquista.setConquista(limpezaDefinitivaConquista);
+                    }
+
+                    grupoConquista.setConquistaConcluida(true);
+                    grupoConquistaRepository.save(grupoConquista);
+                }
+            }
+        } else {
+            throw new RuntimeException("Tarefa não encontrada para o ID: " + id);
+        }
     }
+
 
     @Transactional
     public Tarefa editarPorId(Long id, Tarefa tarefaAtualizada) {
@@ -85,7 +118,34 @@ public class TarefaService {
             if (optionalTarefa.isPresent()) {
                 Tarefa tarefaExistente = optionalTarefa.get();
 
-                // atualiza os campos do produto existente com os valores do produto atualizado
+                // Verifica se é a primeira vez que o grupo está editando uma tarefa
+                Grupo grupo = tarefaExistente.getGrupo();
+                List<Tarefa> tarefasEditadas = tarefaRepository.findByGrupo(grupo).stream()
+                        .filter(t -> !t.getIdTarefa().equals(tarefaExistente.getIdTarefa()) &&
+                                (t.getIsConcluida() || t.getIsRemovida()))
+                        .toList();
+
+                if (tarefasEditadas.size() == 0) {
+                    // Buscar a conquista "Aprimorando"
+                    Conquista aprimorandoConquista = conquistaRepository.findByNome("Aprimorando").orElse(null);
+
+                    if (aprimorandoConquista != null) {
+                        // Verificar se a conquista já existe para o grupo
+                        GrupoConquista grupoConquista = grupoConquistaRepository.findByGrupoAndConquista(grupo, aprimorandoConquista);
+
+                        if (grupoConquista == null) {
+                            // Criar novo registro se não existir
+                            grupoConquista = new GrupoConquista();
+                            grupoConquista.setGrupo(grupo);
+                            grupoConquista.setConquista(aprimorandoConquista);
+                        }
+
+                        grupoConquista.setConquistaConcluida(true);
+                        grupoConquistaRepository.save(grupoConquista);
+                    }
+                }
+
+                // Atualiza os campos da tarefa existente com os valores da tarefa atualizada
                 tarefaExistente.setNome(tarefaAtualizada.getNome());
                 tarefaExistente.setResponsavel(tarefaAtualizada.getResponsavel());
                 tarefaExistente.setDescricao(tarefaAtualizada.getDescricao());
@@ -120,6 +180,7 @@ public class TarefaService {
             Grupo grupo = tarefa.getGrupo();
             List<Tarefa> tarefasConcluidasDoGrupo = tarefaRepository.findByGrupoAndIsConcluida(grupo, true);
 
+            // Lógica para a conquista "Missão Cumprida"
             if (tarefasConcluidasDoGrupo.size() == 1) {
                 // Buscar a conquista "Missão Cumprida"
                 Conquista missaoCumpridaConquista = conquistaRepository.findByNome("Missão Cumprida").orElse(null);
@@ -139,10 +200,72 @@ public class TarefaService {
                     grupoConquistaRepository.save(grupoConquista);
                 }
             }
+
+            // Lógica para a conquista "Em Alta Performance"
+            if (tarefasConcluidasDoGrupo.size() == 10) {
+                // Buscar a conquista "Em Alta Performance"
+                Conquista altaPerformanceConquista = conquistaRepository.findByNome("Em Alta Performance").orElse(null);
+
+                if (altaPerformanceConquista != null) {
+                    // Verificar se a conquista já existe para o grupo
+                    GrupoConquista grupoConquista = grupoConquistaRepository.findByGrupoAndConquista(grupo, altaPerformanceConquista);
+
+                    if (grupoConquista == null) {
+                        // Criar novo registro se não existir
+                        grupoConquista = new GrupoConquista();
+                        grupoConquista.setGrupo(grupo);
+                        grupoConquista.setConquista(altaPerformanceConquista);
+                    }
+
+                    grupoConquista.setConquistaConcluida(true);
+                    grupoConquistaRepository.save(grupoConquista);
+                }
+            }
+
+            if (tarefasConcluidasDoGrupo.size() == 50) {
+                // Buscar a conquista "Produtividade Avançada"
+                Conquista produtividadeAvancadaConquista = conquistaRepository.findByNome("Produtividade Avançada").orElse(null);
+
+                if (produtividadeAvancadaConquista != null) {
+                    // Verificar se a conquista já existe para o grupo
+                    GrupoConquista grupoConquista = grupoConquistaRepository.findByGrupoAndConquista(grupo, produtividadeAvancadaConquista);
+
+                    if (grupoConquista == null) {
+                        // Criar novo registro se não existir
+                        grupoConquista = new GrupoConquista();
+                        grupoConquista.setGrupo(grupo);
+                        grupoConquista.setConquista(produtividadeAvancadaConquista);
+                    }
+
+                    grupoConquista.setConquistaConcluida(true);
+                    grupoConquistaRepository.save(grupoConquista);
+                }
+            }
+
+            if (tarefasConcluidasDoGrupo.size() == 100) {
+                // Buscar a conquista "Mestre das Tarefas"
+                Conquista mesteDasTarefasConquista = conquistaRepository.findByNome("Mestre das Tarefas").orElse(null);
+
+                if (mesteDasTarefasConquista != null) {
+                    // Verificar se a conquista já existe para o grupo
+                    GrupoConquista grupoConquista = grupoConquistaRepository.findByGrupoAndConquista(grupo, mesteDasTarefasConquista);
+
+                    if (grupoConquista == null) {
+                        // Criar novo registro se não existir
+                        grupoConquista = new GrupoConquista();
+                        grupoConquista.setGrupo(grupo);
+                        grupoConquista.setConquista(mesteDasTarefasConquista);
+                    }
+
+                    grupoConquista.setConquistaConcluida(true);
+                    grupoConquistaRepository.save(grupoConquista);
+                }
+            }
         } else {
             throw new RuntimeException("Tarefa não encontrada para o ID: " + id);
         }
     }
+
 
     @Transactional
     public void desmarcarConcluida(Long id) {
@@ -157,12 +280,37 @@ public class TarefaService {
         }
     }
 
+    @Transactional
     public void marcarRemovida(Long id) {
         Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id);
         if (optionalTarefa.isPresent()) {
             Tarefa tarefa = optionalTarefa.get();
             tarefa.setIsRemovida(true);
             tarefaRepository.save(tarefa);
+
+            // Verifica se é a primeira tarefa removida pelo grupo
+            Grupo grupo = tarefa.getGrupo();
+            List<Tarefa> tarefasRemovidasDoGrupo = tarefaRepository.findByGrupoAndIsRemovida(grupo, true);
+
+            if (tarefasRemovidasDoGrupo.size() == 1) {
+                // Buscar a conquista "Descarte Inteligente"
+                Conquista descarteInteligenteConquista = conquistaRepository.findByNome("Descarte Inteligente").orElse(null);
+
+                if (descarteInteligenteConquista != null) {
+                    // Verificar se a conquista já existe para o grupo
+                    GrupoConquista grupoConquista = grupoConquistaRepository.findByGrupoAndConquista(grupo, descarteInteligenteConquista);
+
+                    if (grupoConquista == null) {
+                        // Criar novo registro se não existir
+                        grupoConquista = new GrupoConquista();
+                        grupoConquista.setGrupo(grupo);
+                        grupoConquista.setConquista(descarteInteligenteConquista);
+                    }
+
+                    grupoConquista.setConquistaConcluida(true);
+                    grupoConquistaRepository.save(grupoConquista);
+                }
+            }
         } else {
             throw new RuntimeException("Tarefa não encontrada para o ID: " + id);
         }
